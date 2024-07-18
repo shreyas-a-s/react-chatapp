fb0ea945cbcb2e7c9e8fe9e2c6edab41ec20eaf8
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-18 21:04:48 +0530

    feat: Use the Auth component in App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+import { Auth } from './components/Auth';
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Auth />
+    </div>
+  );
 }
 
 export default App;
