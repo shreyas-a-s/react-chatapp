0debb1981d4be87dbd176f15395ce7165b2a1db4
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-18 21:43:16 +0530

    feat: Build basic chat interface with only an input box

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,3 +1,21 @@
+import { useState } from "react";
+
 export const Chat = () => {
-  return <div className="chat-app">Chat</div>;
+  const [newMessage, setNewMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(newMessage);
+  }
+
+  return <div className="chat-app">
+    <form onSubmit={handleSubmit} className="new-message-form">
+      <input
+        className="new-message-input"
+        placeholder="Type your message here..."
+        onChange={(e) => setNewMessage(e.target.value)}
+      />
+      <button type="submit" className="send-button">Send</button>
+    </form>
+  </div>;
 }
