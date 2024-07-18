3689eba88bfda398058641a4fc06fb9c7b1353b7
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-18 21:08:11 +0530

    feat: Show Auth component only when cookie is present

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { Auth } from './components/Auth';
 
+// cookie imports
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
+
 function App() {
-  return (
-    <div>
-      <Auth />
-    </div>
-  );
+  const [isAuth, setIsAuth] = useState(cookies.get("auth-token"))
+
+  if (!isAuth) {
+    return (
+      <div>
+        <Auth />
+      </div>
+    );
+  }
 }
 
 export default App;
