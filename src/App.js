481b08fe1e84cad4d16aaf2aa314c06d57a8a873
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-18 22:51:50 +0530

    feat: Implement sign out button

--- src/App.js
+++ src/App.js
@@ -7,12 +7,22 @@ import { Chat } from './components/Chat';
 
 // cookie imports
 import Cookies from "universal-cookie";
+import { async } from '@firebase/util';
+import { signOut } from 'firebase/auth';
+import { auth } from './firebase-config';
 const cookies = new Cookies();
 
 function App() {
   const [isAuth, setIsAuth] = useState(cookies.get("auth-token"))
   const [room, setRoom] = useState(null);
   const roomInputRef = useRef(null);
+
+  const signUserOut = async () => {
+    await signOut(auth);
+    cookies.remove("auth-token");
+    setIsAuth(false);
+    setRoom(null);
+  }
 
   if (!isAuth) {
     return (
@@ -23,7 +33,7 @@ function App() {
   }
 
   return (
-    <div>
+    <>
       {room ? (
         <Chat room={room}/>
       ) : (
@@ -33,7 +43,10 @@ function App() {
             <button onClick={() => setRoom(roomInputRef.current.value)}>Enter Chat</button>
           </div>
         )}
-    </div>
+      <div className="sign-out">
+        <button onClick={signUserOut}>Sign Out</button>
+      </div>
+    </>
   )
 }
 
