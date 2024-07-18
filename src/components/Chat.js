6c24d93838ee2e192635963d455cf979d4ab1ef8
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-07-18 22:20:38 +0530

    feat: Query db and show all the messages from same room

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { addDoc, collection, onSnapshot, query, serverTimestamp, where } from "firebase/firestore";
 import { auth, db } from "../firebase-config";
 
 // css
@@ -10,6 +10,20 @@ export const Chat = (props) => {
 
   const [newMessage, setNewMessage] = useState("");
   const messagesRef = collection(db, "messages");
+  const [messages, setMessages] = useState([]);
+
+  useEffect (() => {
+    const queryMessages = query(messagesRef, where("room", "==", room))
+    const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id })
+      });
+      setMessages(messages);
+    });
+
+    return () => unsubscribe();
+  });
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -24,6 +38,7 @@ export const Chat = (props) => {
   }
 
   return <div className="chat-app">
+    <div>{messages.map((message) => <h1>{message.text}</h1>)}</div>
     <form onSubmit={handleSubmit} className="new-message-form">
       <input
         className="new-message-input"
